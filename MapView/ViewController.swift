2602e51f149c74a50de8cf4025797ad591af9b61
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2018-01-02 20:21:00 +0800

    Initial finished files

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var latitude = 14.586936
+    var longitude = 121.104625
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let pinLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let pinObject = MKPointAnnotation()
+        
+        pinObject.coordinate = pinLocation
+        pinObject.title = "SM City East Ortigas"
+        pinObject.subtitle = "Ortigas Ave Ext, Pasig, Metro Manila, Philippines"
+        
+        mapView.addAnnotation(pinObject)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func standard(_ sender: Any) {
+        mapView.mapType = MKMapType.standard
+    }
+    
+    @IBAction func satellite(_ sender: Any) {
+        mapView.mapType = MKMapType.satellite
+    }
+    
+    @IBAction func hybrid(_ sender: Any) {
+        mapView.mapType = MKMapType.hybrid
+    }
+    
+    @IBAction func locateMe(_ sender: Any) {
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0] as CLLocation
+        
+        manager.stopUpdatingLocation()
+        
+        let location = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    @IBAction func directions(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://maps.apple.com/maps?daddr=\(latitude),\(longitude)")!, options: [:], completionHandler: nil)
+        
+    }
 }
 
